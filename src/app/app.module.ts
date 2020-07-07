f3e9ac5d10daeab32a945d65636f888a13334a42
Denis Putnam <denisputnam@verizon.net>
2020-07-07 12:07:09 -0400

    Initial skeleton coded.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,50 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// import { MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { HomeComponent } from './home/home.component';
+import { AccountsComponent } from './accounts/accounts.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { CreateContactComponent } from './create-contact/create-contact.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { CreateActivityComponent } from './create-activity/create-activity.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountsComponent,
+    CreateAccountComponent,
+    ContactsComponent,
+    CreateContactComponent,
+    ActivitiesComponent,
+    CreateActivityComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
