e37bdb57dc26926f159244e04e2f136f1d289c89
Denis Putnam <denisputnam@verizon.net>
2020-07-10 06:08:12 -0400

    switched from sass to scss.

--- src/app/create-activity/create-activity.component.ts
+++ src/app/create-activity/create-activity.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-activity',
   templateUrl: './create-activity.component.html',
-  styleUrls: ['./create-activity.component.sass']
+  styleUrls: ['./create-activity.component.scss']
 })
 export class CreateActivityComponent implements OnInit {
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
